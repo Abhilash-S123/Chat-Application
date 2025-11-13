bba58346be2687880c66dfd44abf6f2a7954039d
Abhilash S <abhilashsubhash72@gmail.com>
2025-11-13 13:58:48 +0530

    Started server and updated package.json

--- server/server.js
+++ server/server.js
@@ -1,1 +1,25 @@
-import 
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import http from 'http';
+import { log } from 'console';
+
+// Create Express app and HTTP server
+const app = express();
+const server = http.createServer(app)
+
+// Middleware setup
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.use("/api/status", (req, res) => {
+    res.send("Server is live")
+})
+
+const PORT = process.env.PORT || 5000;
+
+server.listen( PORT, () => {
+    console.log("server is running on " + PORT);
+})
+
+export default app;
