0bee47efd7c207fe444fdc98186be21d96a63299
Abhilash S <abhilashsubhash72@gmail.com>
2025-11-14 14:02:07 +0530

    Added user authentication in server

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -34,10 +34,31 @@ export const signup = async (req, res) => {
 }
 
 // Controller to login a user
-export const login = (req, res) => {
+export const login = async (req, res) => {
     try {
-        
-    } catch (error) {
-        
+       const {email, password} = req.body;
+       const userData = await User.findOne({email})
+
+       const isPasswordCorrect = await bcrypt.compare(password, userData.password)
+
+       if (!isPasswordCorrect) {
+        return res.json({success: false, message: "Invalid crendentials"})
+       }
+
+       const token = generateToken(userData._id)
+
+        res.json({success: true, userData, token, message: 
+            "Login successful"})
+
+    } catch (error) {   
+        console.log(error.message);
+        res.json({success:false, message: error.message}) 
     }
 }
+
+// Controller to check if user is authenticated
+export const checkAuth = (req, res) => {
+    res.json({success: true, user: req.user})
+}
+
+ 
