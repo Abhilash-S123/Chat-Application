ea8954535fe6080dc9fe968043c51f13f9c99feb
Abhilash S <abhilashsubhash72@gmail.com>
2025-11-22 19:13:46 +0530

    Added chat functionality

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -7,7 +7,7 @@ import { ChatContext } from '../../context/ChatContext'
 const SideBar = () => {
 
    const { getUsers, selectedUser, setSelectedUser, users, 
-       unseenMessages, setunseenMessages} = useContext(ChatContext)
+       unseenMessages, setUnseenMessages} = useContext(ChatContext)
 
    const { logout, onlineUsers } = useContext(AuthContext)
 
@@ -48,7 +48,8 @@ const SideBar = () => {
         </div> 
         <div className='flex flex-col'>
             {filteredUsers.map((user, index) => (
-                 <div onClick={() => {setSelectedUser(user)}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded-2xl
+                 <div onClick={() => {setSelectedUser(user); setUnseenMessages(prev => 
+                    ({...prev, [user._id]: 0}))}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded-2xl
                  cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
                     <img src={user?.profilePic || assets.avatar_icon} alt=""
                      className='w-[35px] aspect-square rounded-full ' />
