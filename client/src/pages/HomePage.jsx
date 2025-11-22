bdba75087e7982d198c6f78a3059ecb2b2c8ffe0
Abhilash S <abhilashsubhash72@gmail.com>
2025-11-22 21:06:47 +0530

    Added rightside bar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import ChatContainer from '../components/ChatContainer'
 import SideBar from '../components/SideBar'
 import RightSideBar from '../components/RightSideBar'
+import { useContext } from 'react'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
 
-    const [selectedUser, setSelectedUser] = useState(false)
+  const {selectedUser} = useContext(ChatContext) 
    
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
@@ -13,7 +15,7 @@ const HomePage = () => {
        overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2' }`}>
         <SideBar/>     
         <ChatContainer/>
-        <RightSideBar selectedUser = {selectedUser} setSelectedUser = {setSelectedUser} />
+        <RightSideBar/>
       </div>          
     </div>
   ) 
